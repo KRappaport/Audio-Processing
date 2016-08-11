14ef29c6af8d8eef6701fbf1ae9f35b5bf6132d7
KRappaport <ymr994@gmail.com>
2016-08-11 22:58:37 +0300

    Added support for multiple cahnnels

--- readwav.c
+++ readwav.c
@@ -17,15 +17,15 @@ typedef struct wave
 	char Subchunk2ID[5];
 	int Subchunk2Size;
 	short int *data_2;
-	int *data_4;
+	short int **data_4;
 }WAVE_FILE;
 
 int main(int argc, char const *argv[])
 {
 	FILE *audio;
 	WAVE_FILE wave_deconstructed;
-	int data_size;
-	audio = fopen("sin.wav", "rb");
+	int data_size, i, k;
+	audio = fopen("Flute.wav", "rb");
 	if (audio==NULL)
 	{
 		printf("Couldn't open:\n");
@@ -46,20 +46,35 @@ int main(int argc, char const *argv[])
 	fread(wave_deconstructed.Subchunk2ID, sizeof(char), 4, audio);
 	fread(&wave_deconstructed.Subchunk2Size, sizeof(int), 1, audio);
 
-	data_size = wave_deconstructed.Subchunk2Size/(int)wave_deconstructed.BlockAlign;
 
-	if (wave_deconstructed.BlockAlign == 2) {
-		wave_deconstructed.data_2 = (short*)malloc(sizeof(short)*data_size);
-		if (wave_deconstructed.data_2==NULL) {
-			perror("Error");
-			exit(1);
+	if (wave_deconstructed.BitsPerSample == 16) {
+
+		data_size = wave_deconstructed.Subchunk2Size/(int)wave_deconstructed.BlockAlign;
+
+		if (wave_deconstructed.BlockAlign == 2) {
+			wave_deconstructed.data_2 = (short*)malloc(sizeof(short)*data_size);
+			if (wave_deconstructed.data_2==NULL) {
+				perror("Error");
+				exit(1);
+			}
+			fread(wave_deconstructed.data_2, sizeof(short), data_size, audio);
+
+		}
+
+		else if (wave_deconstructed.BlockAlign == 4){
+			wave_deconstructed.data_4 = (short**)malloc(sizeof(short)*wave_deconstructed.NumChannels);
+			for (i = 0; i < wave_deconstructed.NumChannels; i++) {
+				wave_deconstructed.data_4[i] = (short*)malloc(sizeof(short)*data_size);
+			}
+			for (i = 0; i < data_size; i++) {
+				for (k = 0; k < wave_deconstructed.NumChannels; k++) {
+					fread(&wave_deconstructed.data_4[k][i], sizeof(short), 1, audio);
+				}
+			}
+
 		}
 	}
 
-
-	fread(wave_deconstructed.data_2, sizeof(short), data_size, audio);
-
-	printf("%d\t%d\t%d\t%d\n", wave_deconstructed.data_2[0], wave_deconstructed.data_2[1], wave_deconstructed.data_2[62], wave_deconstructed.data_2[1000]);
 
 	fclose(audio);
 
